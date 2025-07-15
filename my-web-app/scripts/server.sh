7071f361e59cfbfc7f6fa8a74a0aee995f07a0be
kalaiyarasi1511 <kalaiyarasipanneerselvam1711@gmail.com>
2025-07-15 19:06:01 +0530

    Update server.sh

--- my-web-app/scripts/server.sh
+++ my-web-app/scripts/server.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-
-# Stop Apache (in case it's already running)
-sudo systemctl stop httpd
-
-# Copy files to Apache's public directory
-sudo cp -r /home/ec2-user/my-web-app/* /var/www/html/
-
-# Start Apache again
-sudo systemctl start httpd
-
-# Enable Apache on boot (optional)
-sudo systemctl enable httpd
+echo "Hello from CodeDeploy" > /var/www/html/index.html
