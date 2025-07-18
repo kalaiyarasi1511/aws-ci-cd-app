40adb974ad7cea6046c6d4781dbee6f660f5df7a
kalaiyarasi1511 <kalaiyarasipanneerselvam1711@gmail.com>
2025-07-18 18:49:32 +0530

    Update before_install.sh

--- my-web-app/scripts/before_install.sh
+++ my-web-app/scripts/before_install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-echo "Running BeforeInstall script"
-yum update -y
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
+set -e  # stop on error
+
+echo "======== BeforeInstall: Starting ========"
+sudo yum update -y
+sudo yum install -y httpd
+
+echo "Starting and enabling Apache (httpd)..."
+sudo systemctl start httpd
+sudo systemctl enable httpd
+echo "======== BeforeInstall: Completed ========"
+
